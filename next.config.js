9a0f55b9f125f0a6dd306f177855354bfabcee11
Joseph Whiteaker III <47674962+josephaw1022@users.noreply.github.com>
2022-06-19 12:47:12 -0400

    add environmental variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        MONGO_DB_URL: process.env.MONGO_DB_URL
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
