d69f689f01bbbd1fc0531fd71166286ee62acb78
Joseph Whiteaker III <47674962+josephaw1022@users.noreply.github.com>
2022-06-19 09:42:28 -0400

    format project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-
-
-
 export default function HomePage() {
- 
     return (
-        <div  className=" h-full w-full justify-center items-center flex flex-1 min-h-screen  bg-blue-500 text-red-500  ">
+        <div className=" h-full w-full justify-center items-center flex flex-1 min-h-screen  bg-blue-500 text-red-500  ">
             <h1 className="">Hello Next.js</h1>
         </div>
     );
